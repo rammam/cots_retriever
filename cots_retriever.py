8e72e4184bac34cc4b81fc37e043be9d47eeaef4
rammam <79485135+rammam@users.noreply.github.com>
2022-03-30 19:21:16 +0200

    Add files via upload

---
+++ cots_retriever.py
@@ -0,0 +1,33 @@
+import winreg
+
+def foo(hive, flag):
+    aReg = winreg.ConnectRegistry(None, hive)
+    aKey = winreg.OpenKey(aReg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+                          0, winreg.KEY_READ | flag)
+
+    count_subkey = winreg.QueryInfoKey(aKey)[0]
+
+    software_list = []
+
+    for i in range(count_subkey):
+        software = {}
+        try:
+            asubkey_name = winreg.EnumKey(aKey, i)
+            asubkey = winreg.OpenKey(aKey, asubkey_name)
+            software['name'] = winreg.QueryValueEx(asubkey, "DisplayName")[0]
+
+            try:
+                software['version'] = winreg.QueryValueEx(asubkey, "DisplayVersion")[0]
+            except EnvironmentError:
+                software['version'] = ''
+            software_list.append(software)
+        except EnvironmentError:
+            continue
+
+    return software_list
+
+software_list = foo(winreg.HKEY_LOCAL_MACHINE, winreg.KEY_WOW64_32KEY) + foo(winreg.HKEY_LOCAL_MACHINE, winreg.KEY_WOW64_64KEY) + foo(winreg.HKEY_CURRENT_USER, 0)
+
+for software in software_list:
+    print('%s %s' % (software['name'], software['version']))
+print('Number of installed apps: %s' % len(software_list))
